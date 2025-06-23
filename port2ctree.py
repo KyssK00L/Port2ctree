9a7233bca4ae562bdba9714f61dde70e10d77a49
KyssK00L <jopauc@gmail.com>
2025-06-23 21:31:26 +0200

    fix(output): mark root node as rich text

--- port2ctree.py
+++ port2ctree.py
@@ -24,7 +24,7 @@ def generate_ctd(ports, output_file):
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         f.write('<cherrytree>\n')
-        f.write('  <node name="Open Ports" read_only="false" custom_icon_id="0">\n')
+        f.write('  <node name="Open Ports" read_only="false" custom_icon_id="0" type="rich_text">\n')
         f.write('    <rich_text><![CDATA[]]></rich_text>\n')
         for port, proto, service in ports:
             f.write(f'    <node name="Port {port}/{proto} - {service}" read_only="false" custom_icon_id="0" type="rich_text">\n')
