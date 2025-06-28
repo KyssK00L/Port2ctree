3d17ad05a205f32cdb1e9d76f4056b2ef17ffd5c
KyssK00L <jopauc@gmail.com>
2025-06-28 19:17:28 +0200

    build: add pyproject and improve setup

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='port2ctree',
-    version='1.0',
-    py_modules=['port2ctree'],
+    name="port2ctree",
+    version="1.0",
+    py_modules=["port2ctree"],
     entry_points={
-        'console_scripts': [
-            'port2ctree=port2ctree:main',
+        "console_scripts": [
+            "port2ctree=port2ctree:main",
         ],
     },
-    author='kyssK00l',
-    description='Convertit un scan Nmap/Rustscan en .ctb pour Cherrytree',
+    author="kyssK00l",
+    description="Convert Nmap or Rustscan results into a Cherrytree (.ctd) file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
 
